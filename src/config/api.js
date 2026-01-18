6b1a505b0ad26c7c0ffa6e3790ca68a36a7b920d
Pankaj Singh <pankajsingh10967@gmail.com>
2026-01-18 09:14:45 +0530

    SOS integrated

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getToken } from "../utils/auth";
 
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "http://localhost:5001";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
